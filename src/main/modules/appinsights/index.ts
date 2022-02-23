009e04ff931a4e39beb5367db2aad15c6ff4651e
Scaffolder <scaffolder@backstage.io>
2022-02-23 14:37:24 +0000

    Initial commit

---
+++ src/main/modules/appinsights/index.ts
@@ -0,0 +1,15 @@
+import config from 'config';
+
+const appInsights = require('applicationinsights');
+
+export class AppInsights {
+  enable(): void {
+    if (config.get('appInsights.instrumentationKey')) {
+      appInsights.setup(config.get('appInsights.instrumentationKey')).setSendLiveMetrics(true).start();
+
+      appInsights.defaultClient.context.tags[appInsights.defaultClient.context.keys.cloudRole] =
+        'labs-LH';
+      appInsights.defaultClient.trackTrace({ message: 'App insights activated' });
+    }
+  }
+}
